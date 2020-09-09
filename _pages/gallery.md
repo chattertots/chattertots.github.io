42e1876e97ea4589887e94deb5e8c48bf27cd3a1
Dan Hibbert <dan.hibbert@linklaters.com>
2020-09-09 09:21:03 +0100

    added site and small updates to content

--- _pages/gallery.md
+++ _pages/gallery.md
@@ -17,20 +17,20 @@ header:
 #  overlay_image: /assets/images/page-features/webp/60/about-abstract.webp
 #  caption: "Photo credit: [**Steve Johnson**](https://www.pexels.com/@steve)"
 gallery:
-  - url: /assets/images/gallery/my-home-office.png.jpg
-    image_path: /assets/images/gallerymy-home-office.png.jpg
+  - url: /assets/images/gallery/my-home-office.webp
+    image_path: /assets/images/gallery/my-home-office.webp
     alt: "image of my office"
     title: "My office"
-  - url: /assets/images/gallery/working-from-my-office.jpg
-    image_path: /assets/images/gallery/working-from-my-office.jpg
+  - url: /assets/images/gallery/working-from-my-office.webp
+    image_path: /assets/images/gallery/working-from-my-office.webp
     alt: "image of my 'other office' "
     title: "My 'other office'"
-  - url: /assets/images/gallery/teletherapy-before-green-screen.jpg
-    image_path: /assets/images/gallery/teletherapy-before-green-screen.jpg
+  - url: /assets/images/gallery/teletherapy-before-green-screen.webp
+    image_path: /assets/images/gallery/teletherapy-before-green-screen.webp
     alt: "image of start of teletherapy"
     title: "Before teletherapy starts..."
-  - url: /assets/images/gallery/teletherapy-after-green-screen.jpg
-    image_path: /assets/images/gallery/teletherapy-after-green-screen.jpg
+  - url: /assets/images/gallery/teletherapy-after-green-screen.webp
+    image_path: /assets/images/gallery/teletherapy-after-green-screen.webp
     alt: "image of teletherapy with green screen magic"
     title: "...when the greenscreen goes on"
 
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,70 @@
+var store = [,{
+      "title": "Page Not Found",
+      "excerpt":"Sorry, but the page you were trying to view does not exist — perhaps you can try searching for it below.       ","url": "https://www.chattertots.net/404.html"
+    },{
+      "title": "About Chattertots",
+      "excerpt":"Chattertots is run by Louise Hibbert, a speech and language therapist with over 20 years experience. She worked for several years living in London and working for the NHS running busy early years community clinics, in Special Schools and mainstream units. Chattertots Speech and Language Therapy was set up in...","url": "https://www.chattertots.net/about/"
+    },{
+      "title": "More about Alison",
+      "excerpt":"Alison qualified from Reading University in 2004. She worked for nearly 7 years in the NHS during which time she developed skills and experience in a range of special schools/units and community clinics. Since having her own children she has worked independently. Alison provides assessment, advice and therapy for children...","url": "https://www.chattertots.net/alison/"
+    },{
+      "title": "Posts by Category",
+      "excerpt":" ","url": "https://www.chattertots.net/categories/"
+    },{
+      "title": "Contact",
+      "excerpt":"You can phone or email Chattertots with the links in the left hand menu or use our contact form below   Please don’t include any sensitive information that you don’t want to share with Chattertots.           Your Name:                Your phone number: \t\t\t           Your email:                How can we help you?:                        Send  ","url": "https://www.chattertots.net/contact/"
+    },{
+      "title": "Covid-19",
+      "excerpt":"If you’re concerned about your child’s progress, particularly with the new school year looming, don’t feel you can’t do anything about it until this is well and truly over. Get in touch and we’ll arrange an assessment via ‘teletherapy’. We are offering a FREE FACE-TO-FACE FOLLOW UP SESSION for all...","url": "https://www.chattertots.net/covid/"
+    },{
+      "title": "Fees",
+      "excerpt":"Before committing to an assessment, you are welcome to contact me by phone or by email for a brief discussion of your concerns, and whether Speech and Language Therapy could help. There is no fee for general emails and telephone calls. Initial assessment Time : 1 – 1 ½ hour...","url": "https://www.chattertots.net/fees/"
+    },{
+      "title": "Chattertots at work",
+      "excerpt":"                                                                                                                                                                  ","url": "https://www.chattertots.net/gallery/"
+    },{
+      "title": "Chattertots Speech & Language Therapy",
+      "excerpt":"COVID-19 - we are still operating! If you’re concerned about your child’s progress, particularly with the new school year looming, don’t feel you can’t do anything about it until this is well and truly over. Get in touch and we’ll arrange an assessment via ‘teletherapy’. Learn more Why ? “Thank...","url": "https://www.chattertots.net/"
+    },{
+      "title": "Louise Hibbert",
+      "excerpt":"Louise is a fully qualified, specialist Speech and Language Therapist with 20 years experience. Education I completed my undergraduate degree in Linguistics in 1997. I then qualified as a Speech and Language Therapist in 2001, obtaining a Master of Medical Science (MMedSci) degree in Clinical Communication Studies from the University...","url": "https://www.chattertots.net/louise/"
+    },{
+      "title": "Chattertots Speech & Language Therapy",
+      "excerpt":"{% include feature_row id=\"feature_row_covid\" type=\"center\" %}  {% include feature_row id=\"feature_row_who_what_why\" %}  {% include gallery type=\"center\" %} ","url": "https://www.chattertots.net/offline.html"
+    },{
+      "title": "Personal data request",
+      "excerpt":"Please use this form to request to view/edit/delete personal information that Chattertots may hold           Your Name:                Your phone number: \t\t\t           Your email:                How can we help you?:                        Send ","url": "https://www.chattertots.net/privacy-request/"
+    },{
+      "title": "Privacy Policy",
+      "excerpt":"PRIVACY NOTICE Last updated April 04, 2020 Thank you for choosing to be part of our community at Chattertots Independent Speech and Language Therapy, doing business as Chattertots Speech Therapy (“Chattertots Speech Therapy”, “we”, “us”, or “our”). We are committed to protecting your personal information and your right to privacy....","url": "https://www.chattertots.net/privacy/"
+    },{
+      "title": "Qualifications",
+      "excerpt":"Education I completed my undergraduate degree in Linguistics in 1997 . I then qualified as a Speech and Language Therapist in 2001, obtaining a Master of Medical Science (MMedSci) degree in Clinical Communication Studies from the University of Sheffield. I am now a member of the Royal College of Speech...","url": "https://www.chattertots.net/qualifications/"
+    },{
+      "title": "Additional specific techniques offered",
+      "excerpt":"These are techniques or services that can only be used by a Speech and Language Therapist once they have undergone specific training. PROMPT [Louise](/louise) was lucky enough to be part of the first group of Speech and Language Therapists to become PROMPT trained in the UK. I currently hold a...","url": "https://www.chattertots.net/specialisms/"
+    },{
+      "title": "More about Stephen",
+      "excerpt":"Autonomous practitioner with excellent connections to local highly specialist therapists. Recently spent 2 years working for a large team in a private school supporting children with ASD, language disorders, ADHD and dyspraxia. Available to assess, diagnose and provide therapy for various speech and language needs including stammering/dysfluency. Also experienced working...","url": "https://www.chattertots.net/stephen/"
+    },{
+      "title": "Posts by Tag",
+      "excerpt":"","url": "https://www.chattertots.net/tags/"
+    },{
+      "title": "Teletherapy",
+      "excerpt":"Louise is able to provide Teletherapy services to clients nationwide. This includes teletherapy sessions in school or nursery, provided a teaching assistant can be present to support the child. Teletherapy has proven to be a really successful, and even advantageous, tool during the Cornonovirus pandemic and now Louise is please...","url": "https://www.chattertots.net/teletherapy/"
+    },{
+      "title": "Terms and Conditions",
+      "excerpt":"If you verbally commit to a speech and language therapy sessions, you are agreeing to accept the terms and conditions below. Please read them carefully and do not hesitate to ask any questions that may arise from them. Cancellations 24 hours notice is required. We reserve the right to charge...","url": "https://www.chattertots.net/terms-and-conditions/"
+    },{
+      "title": "Testimonials",
+      "excerpt":"> \"Thank you for everything you have done for H. The change we have seen is amazing and we cannot thank you enough!\" > \"Thank you so much for your help with F. Over the last few weeks, you have been so patient and encouraging. We now can't stop him...","url": "https://www.chattertots.net/testimonials/"
+    },{
+      "title": "Thank you",
+      "excerpt":"Thank you, your message was sent successfully.  We will be in touch soon   ","url": "https://www.chattertots.net/thanks/"
+    },{
+      "title": "What to expect",
+      "excerpt":"Referrals can come from a range of people including parents/carers, Health Visitors, Doctors, Educational staff. However by far the most common course of action is that parents contact us themselves either because they have been concerned, or because somebody has advised them to seek a Speech and Language Therapist. Often...","url": "https://www.chattertots.net/what-to-expect/"
+    },{
+      "title": "Blog",
+      "excerpt":"","url": "https://www.chattertots.net/posts/"
+    },]
